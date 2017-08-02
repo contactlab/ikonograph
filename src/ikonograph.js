5f7824068396fdd1d11f5a92bc407191315dc94a
Mattia Astorino <mattia.astorino@contactlab.com>
2017-08-02 12:31:35 +0200

    chore: Rename Width with size

--- src/ikonograph.js
+++ src/ikonograph.js
@@ -1,5 +1,5 @@
 const ICONS_SVG = {
-  "agenda-full": "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\">\n<title>agenda-full</title>\n<path d=\"M16 0h-13c-0.553 0-1 0.447-1 1v22c0 0.553 0.447 1 1 1h13v-24zM9 7l-3 2v-7h6v7l-3-2z\"></path>\n<path d=\"M19 0h-1v24h1c1.654 0 3-1.346 3-3v-18c0-1.654-1.346-3-3-3z\"></path>\n</svg>\n"
+  "agenda-full": "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" size=\"24\" height=\"24\" viewBox=\"0 0 24 24\">\n<title>agenda-full</title>\n<path d=\"M16 0h-13c-0.553 0-1 0.447-1 1v22c0 0.553 0.447 1 1 1h13v-24zM9 7l-3 2v-7h6v7l-3-2z\"></path>\n<path d=\"M19 0h-1v24h1c1.654 0 3-1.346 3-3v-18c0-1.654-1.346-3-3-3z\"></path>\n</svg>\n"
 };
 
 const MIME_TYPE = 'image/svg+xml';
@@ -17,23 +17,23 @@ class IkonographIcon extends HTMLElement {
 
     this.attributeChangeManager = {
       'icon': value => this._setIcon(value),
-      'width': value => this._setWidth(value)
+      'size': value => this._setSize(value)
     };
   }
 
   static get observedAttributes() {
-    return ['width', 'icon'];
+    return ['size', 'icon'];
   }
 
   connectedCallback() {
     const iconName = this.getAttribute('icon');
-    const width = this.getAttribute('width');
+    const size = this.getAttribute('size');
 
     const svgElement = new DOMParser().parseFromString(ICONS_SVG[iconName], MIME_TYPE);
-    this._setSVGDImensions(svgElement.querySelector('svg'), width);
+    this._setSVGDImensions(svgElement.querySelector('svg'), size);
     this.shadow.appendChild(svgElement.documentElement);
 
-    this.styleElement.innerHTML = this._svgStyleString(width);
+    this.styleElement.innerHTML = this._svgStyleString(size);
     this.shadow.appendChild(this.styleElement);
   }
 
@@ -41,7 +41,7 @@ class IkonographIcon extends HTMLElement {
     this.attributeChangeManager[attributeName](newValue);
   }
 
-  _svgStyleString(width) {
+  _svgStyleString(size) {
     return `
       :host {
         display: -webkit-inline-box;
@@ -53,18 +53,18 @@ class IkonographIcon extends HTMLElement {
 
       svg {
         display: block;
-        width: ${width};
-        height: ${width};
-        stroke-width: 0;
+        size: ${size};
+        height: ${size};
+        stroke-size: 0;
         stroke: currentColor;
         fill: currentColor;
       }
     `;
   }
 
-  _setSVGDImensions(element, width) {
-    element.setAttribute('width', width);
-    element.setAttribute('height', width);
+  _setSVGDImensions(element, size) {
+    element.setAttribute('width', size);
+    element.setAttribute('height', size);
   }
 
   _setIcon(value) {
@@ -73,14 +73,14 @@ class IkonographIcon extends HTMLElement {
       this.shadow.removeChild(currentSVGChild);
       const svgElement = new DOMParser().parseFromString(ICONS_SVG[value], MIME_TYPE);
 
-      const width = this.getAttribute('width');
-      this._setSVGDImensions(svgElement.querySelector('svg'), width);
+      const size = this.getAttribute('size');
+      this._setSVGDImensions(svgElement.querySelector('svg'), size);
 
       this.shadow.appendChild(svgElement.documentElement);
     }
   }
 
-  _setWidth(value) {
+  _setSize(value) {
     const currentSVGChild = this.shadow.querySelector('svg');
     if (currentSVGChild) {
       this._setSVGDImensions(currentSVGChild, value);
